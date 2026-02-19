61e1492e44ba093cfce865f0f70fec9819a3ce64
maxbeny999 <maxbeny999@gmail.com>
2026-02-19 17:52:18 +0900

    15algo, ad miniproject 1차 완성

--- python/Test_ByGem/a.py
+++ python/Test_ByGem/a.py
@@ -1,3 +1,10 @@
-word = "Mississipi"
-word = word.upper()
-print(word)
+n = 30
+lists = []
+perfect_num = []
+for nums in range(1, n + 1):
+    if nums % nums == 0:
+        lists.append(nums)
+        print(lists)
+    if sum(lists[:-1]) == n:
+        perfect_num.append(n)
+print(perfect_num)
